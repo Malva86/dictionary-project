82ade5952938a67bcae32534ccb26f12080b5dda
Malva86 <105357204+Malva86@users.noreply.github.com>
2022-12-19 19:43:12 +0100

    Host at github and netlify

--- src/App.js
+++ src/App.js
@@ -3,7 +3,12 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <p>hello</p>
+      <p>
+        Code created by Maliwina Flis. It is open-sourced at{" "}
+        <a href="https://github.com/Malva86/dictionary-project">GitHub</a> and
+        hosted at{" "}
+        <a href="https://fluffy-parfait-f22f58.netlify.app/">Netlify</a>.
+      </p>
     </div>
   );
 }
