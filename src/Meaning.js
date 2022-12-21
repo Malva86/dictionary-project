188c200b184c7c66769b67a0718b4d929391fa66
Malva86 <105357204+Malva86@users.noreply.github.com>
2022-12-21 15:05:43 +0100

    synonyms fix

--- src/Meaning.js
+++ src/Meaning.js
@@ -17,7 +17,7 @@ export default function Meaning(props) {
               <strong>Example:</strong>
               <em>{definition.example}</em>
               <br />
-              <strong>Synonyms</strong>
+              <strong>Synonyms:</strong>
               <Synonyms synonyms={definition.synonyms} />
             </p>
           </div>
