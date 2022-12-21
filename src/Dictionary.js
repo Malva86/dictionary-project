48d99b8a342b50b5dde715dc615d186371d7bc04
Malva86 <105357204+Malva86@users.noreply.github.com>
2022-12-21 23:50:10 +0100

    img API

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -2,13 +2,20 @@ import React, { useState } from "react";
 import axios from "axios";
 import Results from "./Results";
 import "./Dictionary";
+import Photos from "./Photos";
 
 export default function Dictionary() {
   let [searching, setSearching] = useState(null);
   let [results, setResults] = useState(null);
+  let [photos, setPhotos] = useState(null);
 
   function handleResponse(response) {
     setResults(response.data[0]);
+  }
+
+  function handlePexelsResponse(response) {
+    console.log(response.data);
+    setPhotos(response.data.photos);
   }
 
   function search(event) {
@@ -16,6 +23,11 @@ export default function Dictionary() {
 
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${searching}`;
     axios.get(apiUrl).then(handleResponse);
+
+    let pexelsApiKey = `563492ad6f91700001000001e0e31e9f4dd441829de8d4f1d5cd61f3`;
+    let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${searching}&per_page=9`;
+    let headers = { Authorization: `Bearer ${pexelsApiKey}` };
+    axios.get(pexelsApiUrl, { headers: headers }).then(handlePexelsResponse);
   }
 
   function handleSearchingChange(event) {
@@ -35,6 +47,7 @@ export default function Dictionary() {
         </form>
       </section>
       <Results results={results} />
+      <Photos photos={photos} />
     </div>
   );
 }
